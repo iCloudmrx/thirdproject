21f281593efc5e01f990c01c1ea98ad6ab98608e
iCloudmrx <mahmudjonov2001uz@gmail.com>
2023-02-21 22:08:23 +0500

    there is news
    i added update,delete,edit

--- news_site/articles/urls.py
+++ news_site/articles/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (ArticleListView,
+                    ArticleDeleteView,
+                    ArticleDetailView,
+                    ArticleUpdateView,
+                    ArticleCreateView)
 
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list')
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/detail/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
 ]
--- news_site/articles/views.py
+++ news_site/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, DetailView, CreateView
 from .models import Article
 
 # Create your views here.
@@ -8,3 +9,26 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_update.html'
+    fields = ('title', 'body')
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('author', 'title', 'body')
