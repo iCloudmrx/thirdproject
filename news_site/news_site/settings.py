0813babdd8cb3fb4eba32b4fa110086bcc599568
iCloudmrx <mahmudjonov2001uz@gmail.com>
2023-02-22 12:28:57 +0500

    deploy

--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'article_list'
 
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'article_list'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
